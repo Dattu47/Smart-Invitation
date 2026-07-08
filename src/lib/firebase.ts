c0b6719f5a94ed20f2f5e062703b6ba10f42a7a6
Dattu47 <dattatreya448@gmail.com>
2026-07-08 21:14:52 +0530

    Inject Firebase configuration directly into codebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyD-2plmD0nYr-U-vVwSS1FcCEpFXdpJK34",
+  authDomain: "smart-invitation-47.firebaseapp.com",
+  projectId: "smart-invitation-47",
+  storageBucket: "smart-invitation-47.firebasestorage.app",
+  messagingSenderId: "103851393343",
+  appId: "1:103851393343:web:4dc0511b560f23cb47138a",
+  measurementId: "G-FJM7JSZV99"
 };
 
 // Initialize Firebase
